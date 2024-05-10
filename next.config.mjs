d68bf9f009705945f4bb0f28755229d36cb918d6
baby bin <binhuiyun@gmail.com>
2024-05-09 21:07:48 -0700

    config

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,8 @@
 /** @type {import('next').NextConfig} */
+const nextConfig = {
+    typescript: {
+        ignoreBuildErrors: true,
+    },
+  }
 
-module.exports = {
-  typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
-    ignoreBuildErrors: true,
-  },
-}
+export default nextConfig;
